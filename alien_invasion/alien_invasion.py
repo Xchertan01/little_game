3f6af488d18dd4c650a845c0a68ccaacbe44b951
Xchertan01 <Xchertan@outlook.com>
2021-08-20 15:24:44 +0800

    创建Pygame窗口及相应用户输入

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,48 @@
+# -*- coding = utf-8 -*-
+# @Software: Pycharm
+
+import sys
+
+import pygame
+
+class AlienInvasion:
+    '''管理游戏资源和行为的类'''
+
+    def __init__(self):
+        '''初始化游戏并创建游戏资源'''
+        pygame.init()      #初始化背景设置
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        '''开始游戏的主循环'''
+        while True:
+            # 监视键盘和鼠标事件，为程序响应事件编写事件循环
+            for event in pygame.event.get():
+                # pygame.event.get()返回一个列表，包含其上一次被调用后发生的所有事件
+                if event.type == pygame.QUIT:
+                    sys.exit()            #退出游戏
+
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    # 创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
