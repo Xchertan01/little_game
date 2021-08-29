eb38bc3e5f6d0218d632da323ab96150404f6329
Xchertan01 <Xchertan@outlook.com>
2021-08-29 15:51:50 +0800

    创建一行外星人并修改数值

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -12,10 +12,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         #飞船设置
-        self.ship_speed = 0.5
+        self.ship_speed = 0.7
 
         #子弹设置
-        self.bullet_speed = 0.7
+        self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
