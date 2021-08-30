609d671f7e14845578386669c90f8c107b2a52e9
Xchertan01 <Xchertan@outlook.com>
2021-08-30 20:56:33 +0800

    重构_update_bullets()

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -83,8 +83,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-        # 检查是否有子弹击中了外星人
-        #      如果是，就删除相应的子弹和外星人
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        '''响应子弹和外星人碰撞'''
+        #  删除发生碰撞的子弹和外星人
         collision = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if not self.aliens:
