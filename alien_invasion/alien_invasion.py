24f206aa9e5394f30066258e0aa5ec8cc4354ce1
Xchertan01 <Xchertan@outlook.com>
2021-08-31 18:49:44 +0800

    将Play按钮切换到非活动状态

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -68,8 +68,10 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         '''在玩家单击Play按钮时开始新游戏'''
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # 重置游戏统计信息
+            self.stats.reset_stats()
 
             self.stats.game_active = True
 
