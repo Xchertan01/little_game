45ec45a40521916ba01e66983c026d1ef4f1262b
Xchertan01 <Xchertan@outlook.com>
2021-09-02 15:52:01 +0800

    显示等级

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -78,6 +78,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # 清空余下的外星人和子弹
             self.aliens.empty()
@@ -145,6 +146,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # 提高等级
+            self.stats.level +=1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         '''检查是否有外星人位于屏幕边缘并更新外星人群中所有外星人的位置'''
